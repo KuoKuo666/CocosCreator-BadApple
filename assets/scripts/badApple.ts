7e6677db6cf4f6b5204776d641a6a81271cb723e
yangkuo <2721791961@qq.com>
2021-04-23 00:02:03 +0800

    feat: 导入数据

---
+++ assets/scripts/badApple.ts
@@ -0,0 +1,12 @@
+
+import { _decorator, Component, Node } from 'cc'
+const { ccclass, property } = _decorator
+
+@ccclass('BadApple')
+export class BadApple extends Component {
+
+    start () {
+        
+    }
+
+}
